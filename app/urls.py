6918ecbeed2bd0f005728f5ed5d088a4287a6b13
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-06-18 00:14:06 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('info/<int:pk>/', InformationDetail.as_view(), name='informations-detail'),
     path('infos',Studentinformation.as_view(), name='Studentinfromation'),
     path('students/<int:pk>/', StudentDetails.as_view(), name='student-details'),
-    path('infoc',collagesinformations.as_view(),name ='collagesinformations')
+    path('infoc',collagesinformations.as_view(),name ='collagesinformations'),
 
 ]
